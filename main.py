a4e750a0e242fee7dca18ac69b50b648ad0fe0f1
CS-Sauvaj <hugosimonyjungo@aol.fr>
2021-01-23 18:29:39 +0100

    The water is now red when it reaches the bottom of the concrete

--- main.py
+++ main.py
@@ -12,15 +12,21 @@ import random
 import os.path
 from os import path
 
+
 # -------------------------------------------------------------------------------------------------------------
 # variables (n, p and speed can be set by the user)
 
-n = 50          # the size of the squared matrix is n*n
-p = 0.58        # the probability of erosion for each part of the concrete
-speed = 0.01    # the speed of the animation (in seconds)
+class Var:
+    def __init__(self, n=50, p=0.58, speed=0.01):
+        self.n = n          # the size of the squared matrix is n*n
+        self.p = p          # the probability of erosion for each part of the concrete
+        self.speed = speed  # the speed of the animation (in seconds)
 
-concrete = []
-to_treat = []
+        self.concrete = []  # the matrix representing the concrete
+        self.to_treat = []  # the list of the spots of the concrete where some water has to be put
+
+
+var = Var()
 
 
 # -------------------------------------------------------------------------------------------------------------
@@ -32,20 +38,27 @@ class Simulation (threading.Thread):
         self.sec = sec
 
     def run(self):
-        while len(to_treat) != 0:
-            x, y = to_treat[0]
-            to_treat.pop(0)
-            canvas.create_rectangle(x * (600 / n), y * (600 / n), (x + 1) * (600 / n),
-                                    (y + 1) * (600 / n), fill='blue', outline='blue')
+        while len(var.to_treat) != 0:
+            x, y = var.to_treat[0]
+            var.to_treat.pop(0)
+            if y == var.n-1:
+                # the water has reach the bottom of the concrete
+                canvas.create_rectangle(x * (600 / var.n), y * (600 / var.n), (x + 1) * (600 / var.n),
+                                        (y + 1) * (600 / var.n), fill='red', outline='red')
+            else:
+                # the water is still inside the concrete
+                canvas.create_rectangle(x * (600 / var.n), y * (600 / var.n), (x + 1) * (600 / var.n),
+                                        (y + 1) * (600 / var.n), fill='blue', outline='blue')
             for i_ in range(-1, 2):
                 for j_ in range(-1, 2):
                     # only check up, down, on the left and on the right of the actual coordinates
                     # verify if the checked coordinates are not out of bounds
-                    # check if the checked coordinateds represent an eroded concrete hole
-                    if i_ != j_ and i_ != -j_ and 0 <= x + i_ < n and 0 <= y + j_ < n and concrete[x + i_][y + j_] == 1:
+                    # check if the checked coordinates represent an eroded concrete hole
+                    if i_ != j_ and i_ != -j_ and 0 <= x + i_ < var.n and 0 <= y + j_ < var.n \
+                            and var.concrete[x + i_][y + j_] == 1:
                         # put water in the eroded concrete hole
-                        concrete[x + i_][y + j_] = 2
-                        to_treat.append((x + i_, y + j_))
+                        var.concrete[x + i_][y + j_] = 2
+                        var.to_treat.append((x + i_, y + j_))
             # speed of the animation
             time.sleep(self.sec)
         else:
@@ -56,31 +69,58 @@ class Simulation (threading.Thread):
 # -------------------------------------------------------------------------------------------------------------
 # functions
 
-def create_concrete(p):
+def reset_values():
+    # set all values to default
+    var.n = 50
+    var.p = 0.58
+    var.speed = 0.01
+    var.concrete = []
+    var.to_treat = []
+
+    # create the default random concrete and draw it
+    create_concrete(var.p)
+    draw_concrete()
+
+
+def create_concrete(probability):
     # initialize the matrix representing the concrete
     # 0 represents non-eroded concrete
     # 1 represents eroded concrete
     # 2 represents water
-    for i1 in range(n):
-        concrete.append([])
-        for i2 in range(n):
+    for i1 in range(var.n):
+        var.concrete.append([])
+        for i2 in range(var.n):
             r = random.random()
-            if r > p:
+            if r > probability:
                 # this concrete part will not erode with time
-                concrete[i1].append(0)
+                var.concrete[i1].append(0)
             else:
                 # this concrete part will erode with time
-                concrete[i1].append(1)
+                var.concrete[i1].append(1)
         # the first layer of eroded concrete is filled with water
-        if concrete[i1][0] == 1:
-            to_treat.append((i1, 0))
-            concrete[i1][0] = 2
+        if var.concrete[i1][0] == 1:
+            var.to_treat.append((i1, 0))
+            var.concrete[i1][0] = 2
 
 
 def simulation():
     # start the animation
-    s = Simulation(speed)
+    s = Simulation(var.speed)
     s.start()
+
+
+def draw_concrete():
+    # draw the concrete matrix on the window
+    for i in range(var.n):
+        for j in range(var.n):
+            if var.concrete[i][j] == 0:
+                # this is a non-eroded concrete part
+                canvas.create_rectangle(i * (600 / var.n), j * (600 / var.n), (i + 1) * (600 / var.n),
+                                        (j + 1) * (600 / var.n), fill='black', outline='black')
+            else:
+                # this is an eroded concrete part
+                canvas.create_rectangle(i * (600 / var.n), j * (600 / var.n), (i + 1) * (600 / var.n),
+                                        (j + 1) * (600 / var.n), fill='white', outline='white')
 
 
 # -------------------------------------------------------------------------------------------------------------
@@ -115,20 +155,9 @@ start_button.pack(fill=X, side=BOTTOM)
 # -------------------------------------------------------------------------------------------------------------
 # initialization
 
-# create the concrete matrix with the probability set by the user
-create_concrete(p)
-
-# draw the concrete matrix on the window
-for i in range(n):
-    for j in range(n):
-        if concrete[i][j] == 0:
-            # this is a non-eroded concrete part
-            canvas.create_rectangle(i * (600 / n), j * (600 / n), (i + 1) * (600 / n), (j + 1) * (600 / n),
-                                    fill='black', outline='black')
-        else:
-            # this is an eroded concrete part
-            canvas.create_rectangle(i * (600 / n), j * (600 / n), (i + 1) * (600 / n), (j + 1) * (600 / n),
-                                    fill='white', outline='white')
+# create the concrete matrix with the probability set by the user and draw it
+create_concrete(var.p)
+draw_concrete()
 
 # -------------------------------------------------------------------------------------------------------------
 # finishing window
